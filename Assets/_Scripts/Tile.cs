f75625a8a2340d4a4900dc28359a8c14d6d82612
VietTung0801 <tunle0801@gmail.com>
2023-10-07 12:51:55 -0400

    Place / Delete tiles on mouse pos

--- Assets/_Scripts/Tile.cs
+++ Assets/_Scripts/Tile.cs
@@ -1,6 +1,7 @@
-
 using UnityEngine;
 
 public class Tile :MonoBehaviour
 {
+    public float oxygen;
+    public float temperature;
 }
--- Assets/_Scripts/TilesManager.cs
+++ Assets/_Scripts/TilesManager.cs
@@ -3,22 +3,55 @@ using UnityEngine;
 
 public class TilesManager : MonoBehaviour
 {
-    private Dictionary<Vector2Int, Tile> grid = new Dictionary<Vector2Int, Tile>();
-    [SerializeField] private Tile testTile;
-    [SerializeField] private Vector2Int pos;
+    private Dictionary<Vector2Int, GameObject> grid = new Dictionary<Vector2Int, GameObject>();
+    [SerializeField] private Tile tileToPlaceTest;
+
+    private Plane plane = new Plane(Vector3.up, 0);
+  
+        
 
     public void PlaceTile(Tile tile, Vector2Int pos)
     {
-        grid[pos] = tile;
+        DeleteTile(pos);
         var newTile = Instantiate(tile, this.transform);
-        newTile.transform.localPosition = new Vector3(pos.x + 0.5f, 0, pos.y + 0.5f);
+        newTile.transform.localPosition = new Vector3(pos.x, 0, pos.y);
+        grid[pos] = newTile.gameObject;
+    }
+    public void DeleteTile(Vector2Int pos)
+    {
+        if (!grid.ContainsKey(pos)) return;
+        Destroy(grid[pos]);
+        grid[pos] = null;
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha2))
+        HandleMouseInput();
+    }
+
+    private void HandleMouseInput()
+    {
+        if (Input.GetMouseButtonDown(0))
         {
-            PlaceTile(testTile, pos);
+            float distance;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (plane.Raycast(ray, out distance))
+            {
+                var worldPosition = ray.GetPoint(distance);
+                print(worldPosition);
+                PlaceTile(tileToPlaceTest, new Vector2Int(Mathf.RoundToInt(worldPosition.x), Mathf.RoundToInt(worldPosition.z)));
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float distance;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (plane.Raycast(ray, out distance))
+            {
+                var worldPosition = ray.GetPoint(distance);
+                Destroy(grid[new Vector2Int(Mathf.RoundToInt(worldPosition.x), Mathf.RoundToInt(worldPosition.z))]);
+            }
         }
     }
+
 }
