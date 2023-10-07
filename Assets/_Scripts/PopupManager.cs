e2450af90ef11970d62ac0fe014b169bb3b8acc3
Paul Davis <paulcdavis135@gmail.com>
2023-10-07 18:51:25 -0400

    Popup manager to handle rendering popups

--- Assets/_Scripts/PopupManager.cs
+++ Assets/_Scripts/PopupManager.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class PopupManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject TextPopup;
+    public static PopupManager Instance { get; private set; }
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Run the tutorial sequence of text boxes
+    /// </summary>
+    public static void Tutorial()
     {
-        
+
     }
 }
